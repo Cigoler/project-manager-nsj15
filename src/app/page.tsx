dc28790b331dc2d1a10f213cc1efbd454ce871ca
Cigoler <cigoler@gmail.com>
2024-11-26 15:52:58 +0000

    favicons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,13 +27,11 @@ export default function Dashboard() {
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
+                  <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                  <BreadcrumbPage>Welcome to your dashboard</BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
--- src/components/rovtech-logo.tsx
+++ src/components/rovtech-logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
 
 export function RovtechLogo() {
-  return <Image src="/logo.png" alt="Rovtech Logo" width={100} height={100} className="rounded-sm" />
+  return <Image src="/apple-touch-icon.png" alt="Rovtech Logo" width={100} height={100} className="rounded-sm p-2 bg-[#0D2A30]" />
 }
